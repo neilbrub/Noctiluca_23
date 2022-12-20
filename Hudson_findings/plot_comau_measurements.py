1fc3c0cb909f3fd51eeba87ce3b35ae941e1db08
Neil Brubacher <neil.brubacher@hotmail.com>
2022-12-20 09:53:50 -0500

    Removed misleading inter-inlet range sleeves on comau profiles

--- Hudson_findings/plot_comau_measurements.py
+++ Hudson_findings/plot_comau_measurements.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+Reduced Mean Range (RMR) indicates the range of values across
+all inlets surveyed for each depth level. Note that it does NOT
+provide any information on per-inlet variability of measurements.
+"""
+PLOT_RMR = False
+
+
 def main():
     fname = './Hudson_findings/comau_measurements.csv'
     df = pd.read_csv(fname)
@@ -16,29 +24,33 @@ def main():
     temp_ranges = np.array(df['temp_range']) / 2
     axs[0].plot(temps, depths,
         marker='o', markersize=3, linewidth=1, c='k')
-    axs[0].plot(temps - temp_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[0].plot(temps + temp_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[0].fill_betweenx(depths, temps - temp_ranges, temps + temp_ranges, alpha=0.05, color='k')
 
     sals = np.array(df['sal'])
     sal_ranges = np.array(df['sal_range']) / 2
     axs[1].plot(sals, depths,
         marker='o', markersize=3, linewidth=1, c='k')
-    axs[1].plot(sals - sal_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[1].plot(sals + sal_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[1].fill_betweenx(depths, sals - sal_ranges, sals + sal_ranges, alpha=0.05, color='k')
 
     o2s = np.array(df['o2'])
     o2_ranges = np.array(df['o2_range']) / 2
     axs[2].plot(o2s, depths,
         marker='o', markersize=3, linewidth=1, c='k')
-    axs[2].plot(o2s - o2_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[2].plot(o2s + o2_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
-    axs[2].fill_betweenx(depths, o2s - o2_ranges, o2s + o2_ranges, alpha=0.05, color='k')
+
+    if PLOT_RMR:
+        axs[0].plot(temps - temp_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[0].plot(temps + temp_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[0].fill_betweenx(depths, temps - temp_ranges, temps + temp_ranges, alpha=0.05, color='k')
+        
+        axs[1].plot(sals - sal_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[1].plot(sals + sal_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[1].fill_betweenx(depths, sals - sal_ranges, sals + sal_ranges, alpha=0.05, color='k')
+        
+        axs[2].plot(o2s - o2_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[2].plot(o2s + o2_ranges, depths, alpha=0.3, linewidth=0.5, c='k')
+        axs[2].fill_betweenx(depths, o2s - o2_ranges, o2s + o2_ranges, alpha=0.05, color='k')
 
 
     axs[0].set_xlim([9, 16])
-    axs[1].set_xlim([25, 34])
+    axs[1].set_xlim([27.5, 34])
     axs[2].set_xlim([2, 9])
 
     axs[0].set_title("Temperature (°C)")
